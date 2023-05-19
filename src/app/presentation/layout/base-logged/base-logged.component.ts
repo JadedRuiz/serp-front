e3087b673bdeb3e9f6b681176d9871e8ef28afe9
Jaded Enrique Ruiz <jaded.ruiz@yucatan.gob.mx>
2023-05-19 12:16:50 -0600

    BaseLoged finished

--- src/app/presentation/layout/base-logged/base-logged.component.ts
+++ src/app/presentation/layout/base-logged/base-logged.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { trigger, style, animate, transition, state } from '@angular/animations';
+import * as Notiflix from 'notiflix';
 
 @Component({
   selector: 'app-base-logged',
@@ -14,12 +15,12 @@ import { trigger, style, animate, transition, state } from '@angular/animations'
         })),
         transition(':leave', [
           style({transform: 'translateX(0)'}),
-          animate('1500ms', style({transform: 'translateX(-100%)'}))
+          animate('500ms', style({transform: 'translateX(-100%)'}))
         ]),
         transition(':enter', [
           style({transform: 'translateX(-100%)' }),
-          animate('1500ms', style({transform: 'translateX(0)'}))
-        ])     
+          animate('500ms', style({transform: 'translateX(0)'}))
+        ])
       ]
     )
   ]
@@ -29,19 +30,42 @@ export class BaseLoggedComponent {
   //#region [Variables globales]
     column_size = "is-10";
     show_menu = true;
-    
+
   //#endregion
 
   ocultarMenu(){
     if(this.show_menu){
-      this.column_size = "is-10";
+      // this.column_size = "is-10";
       this.show_menu = false;
     }else{
-      this.column_size = "is-12";
+      // this.column_size = "is-12";
       this.show_menu = true;
     }
-    
-  } 
 
-  
+  }
+
+
+  logout(){
+    Notiflix.Confirm.show(
+      'Estás a punto de cerrar sesión',
+      '¿Estás seguro?',
+      'Si',
+      'No',
+      () => {
+        localStorage.removeItem("token");
+        location.reload();
+      },
+      () => {
+
+      },
+      {
+        backOverlay: true,
+        backOverlayColor: 'rgba(0,0,0,0.8)',
+        titleColor: '#f34e4e',
+        okButtonBackground: '#32c682',
+        cancelButtonBackground: '#f34e4e',
+      },
+      );
+  }
+
 }
