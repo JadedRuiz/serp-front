c926ffbd10da80004872e83c4a3a0feb260c4d6d
SamuelCatzin <catzinx@gmail.com>
2023-06-27 17:44:07 -0600

    boton activo

--- src/app/presentation/features/proveedores/proveedores.component.ts
+++ src/app/presentation/features/proveedores/proveedores.component.ts
@@ -16,13 +16,45 @@ export class ProveedoresComponent {
         // console.log(provInput.nativeElement)
       }
     )
-}
-modificarProveedor(){
-  this.provInputs.forEach(
-    provInput =>{
-      provInput.nativeElement.disabled = true
-      // console.log(provInput.nativeElement)
+  }
+  modificarProveedor() {
+    this.provInputs.forEach(
+      provInput => {
+        provInput.nativeElement.disabled = true
+        // console.log(provInput.nativeElement)
+      }
+    )
+  }
+  status = false
+  cambiarEstado() {
+    if (this.status) {
+      this.status = false
+    }
+    else {
+      this.status = true
+    }
+  }
+
+  @ViewChildren('li') elementos!: QueryList<ElementRef>
+  @ViewChildren('bloque') bloques!: QueryList<ElementRef>
+
+  prueba() {
+    this.elementos.forEach((elemento, i) => {
+      this.elementos.forEach(elemento => {
+        elemento.nativeElement.classList.remove('activo')
+      })
+      console.log(elemento)
+      elemento.nativeElement.classList.add('activo')
     }
     )
-}
+
+    this.bloques.forEach(bloque => {
+      this.bloques.forEach(bloque => {
+        bloque.nativeElement.classList.remove('activo')
+      })
+      bloque.nativeElement.classList.add('activo')
+    }
+    )
+  }
+
 }
