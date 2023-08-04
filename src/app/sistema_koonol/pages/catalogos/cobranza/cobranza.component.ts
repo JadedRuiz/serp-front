0a9313370eb03bcebcefb9a3ed60e0d14d71d70d
Edy <94276786+EdySnakee@users.noreply.github.com>
2023-08-03 18:55:40 -0600

    antes de build

--- src/app/sistema_koonol/pages/catalogos/cobranza/cobranza.component.ts
+++ src/app/sistema_koonol/pages/catalogos/cobranza/cobranza.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Client } from 'src/app/models/clients.model';
+import { ClientsService } from 'src/app/services/clients/clients.service';
+import { Component,OnInit } from '@angular/core';
+import { Observable, Subject, Subscription, debounceTime } from 'rxjs';
+import { FormControl, NgForm } from '@angular/forms';
 import { PedidosService } from 'src/app/services/pedidos/pedidos.service';
 
 @Component({
@@ -6,10 +10,11 @@ import { PedidosService } from 'src/app/services/pedidos/pedidos.service';
   templateUrl: './cobranza.component.html',
   styleUrls: ['./cobranza.component.scss'],
 })
-export class CobranzaComponent {
+export class CobranzaComponent implements OnInit{
+  //PEDIDOS
   pedidos: any = [];
   pedidoSeleccionado: any;
-
+ //var CALCULADORA
   b1000: number = 0;
   b500: number = 0;
   b200: number = 0;
@@ -36,15 +41,22 @@ export class CobranzaComponent {
 
 
   //para paginador
-
-
 itemsPerPage =2;
 p=1;
 
-  constructor(private pedidosRealizados: PedidosService) {}
+  constructor(
+    private pedidosRealizados: PedidosService,
+  private clienteService: ClientsService,
+
+    ) {}
 
   ngOnInit() {
     this.obtenerPedidos();
+    this.searchClientControl.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((value) => {
+        this.buscarCliente(value);
+      });
   }
 
   //Para obtener Pedidos
@@ -89,7 +101,79 @@ p=1;
     this.totalIngresosReal = this.totalIngresos - this.totalCambio;
   }
 
+//=>>> BUSCAR CLIENTE
+clients:Client[]=[];
+searchClient: string = '';
+autocompleteClients: any[] = [];
+selectedClient: Client = new Client(0, 0, 1, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 0, 0, 0, 0, 0, 1, 0);
+searchClientSubscription: Subscription = new Subscription();
+isClientSelected: boolean = false;
+searchList: boolean = false;
+loader: boolean = false
+noClients: boolean = false
+searchClientControl: FormControl = new FormControl();
 
+
+//FUNCION PARA HACER BÚSQUEDA DE CLIENTES POR NOMBRE O RFC
+buscarCliente(value: string) {
+  let json = {
+    id_cliente: 0,
+    id_comprador:1,
+    cliente: '',
+    token: '',
+  }
+  if (value.length <= 3) {
+    this.autocompleteClients = [];
+    this.searchList = false;
+  } else if (!this.searchClientSubscription.closed) {
+    this.loader = true;
+    this.searchList = true;
+    this.clienteService.obtenerClientes(json).subscribe(
+      (resp) => {
+        if (resp.ok) {
+          this.clients = resp.data;
+          this.autocompleteClients = this.clients.filter(
+            (client) =>
+              client.cliente.toLowerCase().includes(value.toLowerCase()) ||
+              client.rfc?.toLowerCase().includes(value.toLowerCase())
+          );
+          console.log(this.autocompleteClients);
+          this.loader = false;
+        }
+      },
+      (err) => {
+        console.log(err);
+        this.loader = false;
+      }
+    );
+  }
+}
+
+//FUNCIÓN PARA ESCOGER UN CLIENTE Y GUARDAR SU ID EN addressSelected
+seleccionarCliente(id_cliente: number) {
+  if (id_cliente) {
+    this.selectedClient = this.autocompleteClients.find(
+      (aclient) => aclient.id_cliente === id_cliente
+    );
+    //this.visita.id_cliente = id_cliente;
+    this.isClientSelected = true;
+    this.searchList = false;
+    this.searchClientControl.setValue(this.selectedClient.cliente)
+    this.searchClientSubscription.unsubscribe();
+  } else {
+    return;
+  }
+}
+
+  //Función para que al dar clic en el input nos suscribamos a los cambios del mismo
+  onFocusClientSearch() {
+    this.searchClientSubscription = this.searchClientControl.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((value) => {
+        this.buscarCliente(value);
+      });
+    // console.log("Estás sobre el input: ", this.searchClientSubscription);
+  }
 
 
 }
