c48a195e56434414255632ebdd7ac5bf6af0a50a
Ilaah <104544011+Ilaah@users.noreply.github.com>
2023-08-15 16:00:21 -0600

    [add][frontend] añadiendo cosas a maps

--- src/app/sistema_koonol/pages/catalogos/maps/maps.component.ts
+++ src/app/sistema_koonol/pages/catalogos/maps/maps.component.ts
@@ -15,69 +15,98 @@ export class MapsComponent {
   coords: number[] = [];
   long: number = 0;
   lat: number = 0;
-  center: any;
+  center: any = { lat: 0, lng: 0 }
+  map: any;
+  marker: google.maps.Marker = new google.maps.Marker();
 
   ngOnInit(): void {
+    // console.log(this.haversineDistance(20.98580748584478, -89.59105653507723, this.lugarLat, this.lugarLong));
     this.geolocationService.getUserLocation()
       .then(resp => {
-        console.log(resp);
-        this.coords = resp
-        this.long = this.coords[0]
-        this.lat = this.coords[1]
-        this.center = { lat: this.lat, lng: this.long }
-        this.geolocationService.updatePinLocation([this.long, this.lat])
-        this.initMap()
+        //ASIGNANDO COORDENADAS
+        this.coords = resp;
+        this.long = this.coords[0];
+        this.lat = this.coords[1];
+
+        //CENTRO DEL MAPA
+        this.center = { lat: this.lat, lng: this.long };
+
+        //CREANDO MAPA
+        this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+          zoom: 18.5,
+          center: this.center,
+          mapTypeControl: false,
+        });
+
+        let map = this.map
+        this.marker = new google.maps.Marker({
+          map,
+          draggable: true,
+          position: this.center
+        });
+
+        //ACTUALIZAR LA LOCALIZACIÓN DEL PIN CON LA UBICACIÓN ACTUAL
+        this.geolocationService.updatePinLocation([this.long, this.lat]);
+
+        //INICIALIZAR EL MAPA
+        this.initMap();
+
+        //SUSCRIPCIÓN A LOS CAMBIOS QUE EXISTAN EN LA UBICACIÓN DEL PIN
+        this.geolocationService.pinLocation$.subscribe(data => {
+          this.long = data[0]
+          this.lat = data[1]
+          this.pinPositionChanged()
+        });
       })
   }
 
+
+  //INICIALIZACIÓN DEL MAPA
   initMap(): void {
-    let map: google.maps.Map;
-    let marker: google.maps.Marker;
+    let map: google.maps.Map = this.map
+    let marker: google.maps.Marker = this.marker
 
-    map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-      zoom: 18.5,
-      center: this.center,
-      mapTypeControl: false,
-    });
-
-    marker = new google.maps.Marker({
-      map,
-      draggable: true,
-      position: this.center
+    
+    //FUNCIÓN PARA QUE AL ARRASTRAR EL PIN A ALGUNA PARTE DEL MAPA NOS REDIRIJA ALLÍ Y SE ACTUALICE LA LOCALIZACIÓN DEL PIN
+    marker.addListener('dragend', () => {
+      const newPosition = marker.getPosition()!;
+      const lat = newPosition.lat();
+      const lng = newPosition.lng();
+      map.setCenter(newPosition);
+      this.geolocationService.updatePinLocation([lng, lat]);
     });
 
     //FUNCIÓN PARA QUE AL HACER CLICK EN ALGUNA PARTE DEL MAPA NOS REDIRIJA ALLÍ Y SE ACTUALICE LA LOCALIZACIÓN DEL PIN
-    map.addListener("click", (e: google.maps.MapMouseEvent) => {
-      this.geocode({ location: e.latLng }, map, marker);
+    map.addListener('click', (event: google.maps.MapMouseEvent) => {
+      const newPosition = event.latLng!;
+      map.setCenter(newPosition);
+      marker.setPosition(newPosition);
+      const lat = newPosition.lat();
+      const lng = newPosition.lng();
+      this.geolocationService.updatePinLocation([lng, lat]);
     });
-
-    // this.geolocationService.pinLocation$.subscribe((data) => {
-    //   map.setCenter({lat: data[0], lng: data[1]})
-    //   marker.setPosition({lat: data[0], lng: data[1]})
-    // })
   }
 
-  geocode(request: google.maps.GeocoderRequest, map: google.maps.Map, marker: google.maps.Marker): void {
-    let geocoder: google.maps.Geocoder;
+  pinPositionChanged() {
+    this.marker.setPosition({ lat: this.lat, lng: this.long });
+    this.map.setCenter({ lat: this.lat, lng: this.long });
+    this.map.setZoom(16.5)
+  }
 
-    geocoder = new google.maps.Geocoder();
+  //Comprobar si el usuario se encontraba en un radio de 200 metros del lugar a visitar
+  radio:number = 200
+  lugarLong:number = -89.587277
+  lugarLat:number = 20.9842583
 
-    geocoder
-      .geocode(request)
-      .then((result) => {
-        const { results } = result;
-        console.log(result);
-        map.setCenter(results[0].geometry.location);
-        marker.setPosition(results[0].geometry.location);
-        marker.setMap(map);
-        let lat = results[0].geometry.location.lat()
-        let lng = results[0].geometry.location.lng()
-
-        this.geolocationService.updatePinLocation([lng, lat])
-        return results;
-      })
-      .catch((e) => {
-        alert("Geocode was not successful for the following reason: " + e);
-      });
+  haversineDistance(lat1:number, lon1:number, lat2:number, lon2:number) {
+    const R = 6371000; // Radio de la Tierra en metros
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+    return distance;
   }
 }
